1a13f5567377512b1c713de4aa6cf87f938e253c
Florian <naumannfl79974@th-nuernberg.de>
2020-10-24 18:58:16 +0200

    Tic Tac Toe play box with symbols spawns when clicked.
    - if clicked, a sysmbol repective to its player is spawn in the clicked play box .
    - This click event can only occurs once and  can only betriggerd by one player !

---
+++ PlayerSymbolsControl/TicTacToeBoxControl.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace TicTacToeControl
+{
+    public enum GameState
+    {
+        TurnPlayerOne,
+        TurnPlayerTwo,
+        PlayerOneWins,
+        PlayerTwoWins,
+        Draw
+    }
+
+    
+
+    /// <summary>
+    /// Interaction logic for TicTacToeBoxControl.xaml. Represents a Tic Tac Toe grid
+    /// which can be used by 2 players. It also manages the state of the game, whose turn is
+    /// when and who wins or in which case a draw occurs.
+    /// </summary>
+    public partial class TicTacToeBoxControl : UserControl
+    {
+        private readonly Button[] playFields = new Button[9];
+
+        private GameState stateOfGame = GameState.TurnPlayerOne;
+        public TicTacToeBoxControl()
+        {
+            InitializeComponent();
+
+            // Get References of all playFields to add click events to them.
+            var rootElement = LogicalTreeHelper.GetChildren(this).GetEnumerator();
+            rootElement.MoveNext();
+            var mainStack = rootElement.Current as StackPanel;
+
+            int count = 0;
+            foreach (WrapPanel wrap in mainStack.Children)
+            {
+                foreach (Button contentControl in wrap.Children)
+                {
+                    //playFiels[count] = contentControl;
+                    Button playField = playFields[count++];
+                    playField = contentControl;
+                    playField.Tag = count;
+
+                    playField.Click += PlayField_Click;
+                    Debug.WriteLine(count);
+                    
+                }
+
+
+            }
+           
+        }
+
+        /// <summary> 
+        /// Puts symbol in the play box depending on whose turn is and removes the click event.
+        /// Cross symbol represents player one Circle represents player two.
+        /// </summary>
+        /// <param name="sender"> sender as a button control as play box </param>
+        public void PlayField_Click(object sender, RoutedEventArgs e)
+        {
+            
+            e.Handled = true;
+            
+            if (sender is Button playBox)
+            {
+                if (this.stateOfGame == GameState.TurnPlayerOne)
+                {
+                    playBox.Content = new Cross();
+                    this.stateOfGame = GameState.TurnPlayerTwo;
+
+                } else if (this.stateOfGame == GameState.TurnPlayerTwo)
+                {
+                    playBox.Content = new Circle();
+                    this.stateOfGame = GameState.TurnPlayerOne;
+                }
+
+                // No need to listen to the event anymore. Play field can be selected only once by one player.
+                playBox.Click -= PlayField_Click;
+            }
+
+            
+        }
+
+
+    }
+
+
+}
