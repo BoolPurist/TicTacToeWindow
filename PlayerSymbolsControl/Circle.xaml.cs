a8d2808570ba71b875591d1151e119fb84e11dd1
Florian <naumannfl79974@th-nuernberg.de>
2020-10-24 15:49:55 +0200

    Implemented Circle control

--- PlayerSymbolsControl/Circle.xaml.cs
+++ PlayerSymbolsControl/Circle.xaml.cs
@@ -1,8 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,20 +11,20 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using TicTacToeControl;
 
-
-namespace TicTacToeMainWindow
+namespace TicTacToeControl
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for PlayerCircle.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    
+    public partial class Circle : UserControl
     {
-        public MainWindow()
+        public Circle()
         {
             InitializeComponent();
-            
-        }
+        }                            
     }
+
+    
 }
