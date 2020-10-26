ac41e8d7108cb7e76415aa8cd18b83e5c34d93cd
Florian <naumannfl79974@th-nuernberg.de>
2020-10-26 23:18:05 +0100

    Added model for tictacbox and new project xunit for tictactoe control
    - Gonna test the model for tictactoebox

---
+++ TicTacToeControl/TicTacToeBox/TicTacToeModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeControl.TicTacToeBox
+{
+  public class TicTacToeModel
+  {
+
+  }
+}
---
+++ TicTacToeControl_XUnit/TicTacToeBoxUnit.cs
@@ -0,0 +1,17 @@
+using System;
+
+using Xunit;
+
+using TicTacToeControl;
+
+namespace TicTacToeControl_XUnit
+{
+  public class TicTacToeBoxUnit
+  {
+    [Fact]
+    public void Test1()
+    {
+
+    }
+  }
+}
