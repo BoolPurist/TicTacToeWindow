f2126dd360621c4cd062c065963bd9b3a77c3512
Florian <naumannfl79974@th-nuernberg.de>
2020-10-27 22:10:41 +0100

    Implented MakeTurn method of TicTacToeControl
    - Mapps field number to tic tac toe play box and  assigns the the field to the repective player.
    - Throws exception if field number is not empty anymore.
    - Throws exceptions if field number is above 9 or is negative.
    - Written tests verify these functionallity.

--- TicTacToeControl/TicTacToeBox/TicTacToeModel.cs
+++ TicTacToeControl/TicTacToeBox/TicTacToeModel.cs
@@ -2,10 +2,91 @@
 using System.Collections.Generic;
 using System.Text;
 
+#nullable enable
+
 namespace TicTacToeControl.TicTacToeBox
 {
   public class TicTacToeModel
   {
 
+    public TicTacToeModel()
+    {
+      this.currentState = GameState.TurnPlayerOne;
+      this.fieldGrid = new FieldStatus[3,3];
+
+      for (int i = 0, Columnlength = fieldGrid.GetLength(0); i < Columnlength; i++ )
+      {
+        for (int j = 0, RowLength = fieldGrid.GetLength(1); j < RowLength; j++)
+        {
+          this.fieldGrid[i, j] = FieldStatus.Empty;
+        }
+      }
+     
+        
+    }
+
+    private readonly FieldStatus[,] fieldGrid;
+
+    private const int maximumFieldNumber = 8;
+
+    private enum FieldStatus
+    {
+      Player1Occupied,
+      Player2Occupied,
+      Empty
+    }
+
+    private GameState currentState;
+
+    public GameState MakeTurn(int fieldNumber)
+    {
+      int ColumnNumber = fieldNumber / 3;
+      int RowNumber = fieldNumber % 3;
+      
+      if (fieldNumber < 0)
+      {
+        throw new ArgumentOutOfRangeException
+          (
+          nameof(fieldNumber), fieldNumber,
+          $"Field number must not be negative!"
+          );
+      }
+      else if (fieldNumber > maximumFieldNumber)
+      {
+        throw new ArgumentOutOfRangeException
+          (
+          nameof(fieldNumber), fieldNumber,
+          $"Field number must not be greater than {maximumFieldNumber} !"
+          );
+      }
+      else if (this.fieldGrid[ColumnNumber, RowNumber] != FieldStatus.Empty)
+      {
+        throw new NoEmptyPlayFieldException(
+          nameof(fieldNumber),
+          $"FieldNumber is not empty anymore !",
+          fieldNumber
+          );
+      }
+      else if (this.currentState == GameState.TurnPlayerOne)
+      {
+        this.currentState = GameState.TurnPlayerTwo;
+        this.fieldGrid[ColumnNumber, RowNumber] = FieldStatus.Player1Occupied;
+      }
+      else
+      {
+        this.currentState = GameState.TurnPlayerOne;
+        this.fieldGrid[ColumnNumber, RowNumber] = FieldStatus.Player2Occupied;
+      }
+
+
+
+      return this.currentState;
+    }
+  }
+
+  public class NoEmptyPlayFieldException : ArgumentOutOfRangeException
+  {
+    public NoEmptyPlayFieldException(string paramName , string message, object? actualValue ) 
+      : base(paramName, actualValue, message) { }
   }
 }
--- TicTacToeControl_XUnit/TicTacToeBoxUnit.cs
+++ TicTacToeControl_XUnit/TicTacToeBoxUnit.cs
@@ -3,15 +3,98 @@ using System;
 using Xunit;
 
 using TicTacToeControl;
+using TicTacToeControl.TicTacToeBox;
+using System.Collections.Generic;
 
 namespace TicTacToeControl_XUnit
 {
+
   public class TicTacToeBoxUnit
   {
-    [Fact]
-    public void Test1()
-    {
 
+
+    [Theory]
+    [MemberData(nameof(TurnsNoWinsDraws))]
+    public void MakeTurn_ShouldReturnNextPlayer(int[] madeTurns, GameState[] expectedStates)
+    {    
+      IterCases(madeTurns, expectedStates);
+    }
+      
+    public static TheoryData<int[], GameState[]> TurnsNoWinsDraws 
+      => new TheoryData<int[], GameState[]>()
+        {
+          {
+            new int[] {2, 4, 5 },
+            new GameState[]
+            {
+              GameState.TurnPlayerTwo,
+              GameState.TurnPlayerOne,
+              GameState.TurnPlayerTwo
+            }
+          },
+          {
+            new int[] { 0, 1, 5, 8 },
+            new GameState[]
+            {
+              GameState.TurnPlayerTwo,
+              GameState.TurnPlayerOne,
+              GameState.TurnPlayerTwo,
+              GameState.TurnPlayerOne
+            }
+          },
+          {
+            new int[] { 5, 4, 7, 2, 3 },
+            new GameState[]
+            {
+              GameState.TurnPlayerTwo,
+              GameState.TurnPlayerOne,
+              GameState.TurnPlayerTwo,
+              GameState.TurnPlayerOne,
+              GameState.TurnPlayerTwo
+            }
+          }
+        };
+
+    [Theory]
+    [MemberData(nameof(NegativeOrTooHightFieldNumberData))]
+    public void MakeTurn_ShouldThrowIfFieldNumberNegativeOrTooHigh(
+        int [] madeTurns
+        )
+    {
+      Assert.Throws<ArgumentOutOfRangeException>
+        (
+          () => 
+          {
+            var ticTacToeBox = new TicTacToeModel();
+            foreach (int fieldNumber in madeTurns)
+            {
+              ticTacToeBox.MakeTurn(fieldNumber);
+            }
+          }
+        );  
+    }
+
+    public static TheoryData<int[]> NegativeOrTooHightFieldNumberData
+    => new TheoryData<int[]>()
+    {      
+       new int[] { 2, -1 },
+       new int[] { 0, 2, 12}      
+    };
+
+
+
+
+    private static void IterCases(int[] input, GameState[] expectedOutput)
+    {
+      var ticTacToeBox = new TicTacToeModel();
+
+      for (int i = 0, length = input.Length; i < length; i++)
+      {
+        Assert.Equal(ticTacToeBox.MakeTurn(input[i]), expectedOutput[i]);
+      }
     }
   }
+
+  
+
 }
