a1988dfc9f2ced9e2871eaa23804f5022b1018f4
Florian <naumannfl79974@th-nuernberg.de>
2020-10-28 22:01:59 +0100

    Implemented returning a draw as a state. Added comments above MakeTurn

--- TicTacToeControl/TicTacToeBox/TicTacToeModel.cs
+++ TicTacToeControl/TicTacToeBox/TicTacToeModel.cs
@@ -25,48 +25,80 @@ namespace TicTacToeControl.TicTacToeBox
 
     // TODO Implement returning GameState.draw
     // TODO Implement returning GameState.PlayerOneWins
+    private int turnedCounter = -1;
+    private bool hasEnded = false;
 
+    /// <summary> 
+    /// Processes a made turned and returns the state of the tic tac toe play box
+    /// after the made turn.
+    /// </summary>
+    /// <param name="fieldNumber"> 
+    /// Number of the tic tac toe field from 0 to 8. Counting starts from the top left 
+    /// corner and ends at the bottom right corner of the box
+    /// </param>
+    /// <returns> 
+    /// Returns the state after a turn. The state tells 
+    /// which player is next, has already won or if a draw occurred
+    /// </returns>
+    /// <exception cref="NoEmptyPlayFieldException"> 
+    /// If a field number is given twice for party (Would occupy a non-empty field)
+    /// </exception>
+    /// <exception cref="ArgumentOutOfRangeException"> 
+    /// If field number is above 8 or negative
+    /// </exception>
     public GameState MakeTurn(int fieldNumber)
     {
-      int ColumnNumber = fieldNumber / 3;
-      int RowNumber = fieldNumber % 3;
-      
-      if (fieldNumber < 0)
-      {
-        throw new ArgumentOutOfRangeException
-          (
-          nameof(fieldNumber), fieldNumber,
-          $"Field number must not be negative!"
-          );
-      }
-      else if (fieldNumber > maximumFieldNumber)
-      {
-        throw new ArgumentOutOfRangeException
-          (
-          nameof(fieldNumber), fieldNumber,
-          $"Field number must not be greater than {maximumFieldNumber} !"
-          );
-      }
-      else if (this.fieldGrid[ColumnNumber, RowNumber] != FieldStatus.Empty)
-      {
-        throw new NoEmptyPlayFieldException(
-          nameof(fieldNumber),
-          $"FieldNumber is not empty anymore !",
-          fieldNumber
-          );
-      }
-      else if (this.currentState == GameState.TurnPlayerOne)
-      {
-        this.currentState = GameState.TurnPlayerTwo;
-        this.fieldGrid[ColumnNumber, RowNumber] = FieldStatus.Player1Occupied;
-      }
-      else
-      {
-        this.currentState = GameState.TurnPlayerOne;
-        this.fieldGrid[ColumnNumber, RowNumber] = FieldStatus.Player2Occupied;
-      }
-      
 
+      if (!this.hasEnded)
+      {
+
+        int ColumnNumber = fieldNumber / 3;
+        int RowNumber = fieldNumber % 3;
+
+        if (fieldNumber < 0)
+        {
+          throw new ArgumentOutOfRangeException
+            (
+            nameof(fieldNumber), fieldNumber,
+            $"Field number must not be negative!"
+            );
+        }
+        else if (fieldNumber > maximumFieldNumber)
+        {
+          throw new ArgumentOutOfRangeException
+            (
+            nameof(fieldNumber), fieldNumber,
+            $"Field number must not be greater than {maximumFieldNumber} !"
+            );
+        }
+        else if (this.fieldGrid[ColumnNumber, RowNumber] != FieldStatus.Empty)
+        {
+          throw new NoEmptyPlayFieldException(
+            nameof(fieldNumber),
+            $"FieldNumber is not empty anymore !",
+            fieldNumber
+            );
+        }
+        else if (this.currentState == GameState.TurnPlayerOne)
+        {
+          this.currentState = GameState.TurnPlayerTwo;
+          this.fieldGrid[ColumnNumber, RowNumber] = FieldStatus.Player1Occupied;
+        }
+        else
+        {
+          this.currentState = GameState.TurnPlayerOne;
+          this.fieldGrid[ColumnNumber, RowNumber] = FieldStatus.Player2Occupied;
+        }
+
+        this.turnedCounter++;
+
+        if (turnedCounter == maximumFieldNumber)
+        {
+          this.currentState = GameState.Draw;
+          this.hasEnded = true;
+        }
+
+      }
 
       return this.currentState;
     }
